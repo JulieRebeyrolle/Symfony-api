b6a3ab638f4adeb075bbdb4a79fdc2660ec7a29a
julierebeyrolle <julie.rebeyrolle@gmail.com>
2021-10-03 19:43:23 +0200

    Update ProgramController index http methods

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route ("/program/", name="program_index", methods={"GET"})
+     * @Route ("/", methods={"GET"}, name="index")
      * @return Response
      */
     public function index(): Response
